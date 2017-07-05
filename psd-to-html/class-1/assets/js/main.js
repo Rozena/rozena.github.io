c7e9c63ac3f24333fd5e8b372257c825da3f3e92
Rozy <rrrozyislam@gmail.com>
2017-07-04 23:28:59 -0700

    class-1

---
+++ psd-to-html/class-1/assets/js/main.js
@@ -0,0 +1,87 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+        $(".homepage-slides").owlCarousel({
+           items: 1,
+           dots:true,
+           autoplay: true,
+           loop: true,
+           nav: true,
+           navText: ["<i class=\"zmdi zmdi-long-arrow-left\"></i>", "<i class=\"zmdi zmdi-long-arrow-right\"></i>"], animateOut: "fadeOut",
+           animateIn: 'fadeIn',    
+        });
+        
+        $(".case-studies-carousel").owlCarousel({
+           items: 3,
+           margin: 30,
+           dots:true,
+           autoplay: false,
+           loop: true,
+           nav: false,        
+        });
+        
+        $(".testimonial-carousel").owlCarousel({
+           items: 1,
+           dots:true,
+           autoplay: false,
+           loop: true,
+           nav: true,
+           navText: ["<i class=\"zmdi zmdi-chevron-left\"></i>", "<i class=\"zmdi zmdi-chevron-right\"></i>"],  
+        });
+
+       
+        /*
+        $(".testimonial-carousel").owlCarousel({
+           items: 1,
+           dots:true,
+           autoplay: true,
+           loop: true,
+           nav: true,
+           navText: ["<i class=\"zmdi zmdi-chevron-left\"></i>", "<i class=\"zmdi zmdi-chevron-right\"></i>"],     
+           addClassActive: true,
+           animateOut: 'slideOutDown',
+           animateIn: 'rollIn', 
+        });
+        */
+        
+        $(".brand-logo-carousel").owlCarousel({
+           items: 6,
+           margin: 30,
+           dots:false,
+           autoplay: true,
+           loop: true,
+           nav: false,    
+        });
+        
+      $(".menu-trigger").on("click", function() {
+          $(".off-canvas-menu").addClass("show-off-canvas-menu");
+          $(".off-canvas-menu-overlay").addClass("active");
+      });
+      $(".menu-close, .off-canvas-menu-overlay").on("click", function() {
+          $(".off-canvas-menu").removeClass("show-off-canvas-menu");
+          $(".off-canvas-menu-overlay").removeClass("active");
+      });
+        
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
