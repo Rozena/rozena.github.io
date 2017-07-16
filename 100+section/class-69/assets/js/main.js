cdcd890f4b669dc19ca59f9ea84ed0e61ec2e89e
Rozy <rrrozyislam@gmail.com>
2017-07-15 18:15:45 -0700

    class-69

---
+++ 100+section/class-69/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+       
+    var footerHeight = $(".footer-widget-area").height();
+	$(".main-site-wrapper").css('margin-bottom', footerHeight + 'px');
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
