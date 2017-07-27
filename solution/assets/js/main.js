5810bc0f5db5602f402734f86487ef70e4232488
Rozy <rrrozyislam@gmail.com>
2017-07-27 13:57:36 -0700

    solution

---
+++ solution/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+        $(".search-bar").on('click', function() {
+        	$(".search-form").toggleClass('visible')
+        });
+     
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
