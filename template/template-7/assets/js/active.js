72852bf3570217c61c82254b90c2818816dac5eb
rozena <rrrozyislam@gmail.com>
2017-09-11 02:07:00 +0600

    template-7

--- template/template-7/assets/js/active.js
+++ template/template-7/assets/js/active.js
@@ -3,6 +3,11 @@
 
     jQuery(document).ready(function($) {
 
+
+        $("#mail-chimp").ajaxChimp({
+            url:"http://netlify.us16.list-manage.com/subscribe/post?u=c2469184e53e2b2f2c0ec5601&amp;id=0d51d314af"
+        });
+
         function countdown() {
             var now = new Date();
             var eventDate = new Date(2018, 1, 1);
@@ -24,41 +29,37 @@
             s = (s < 10) ? "0" + s : s;
 
 
-           document.getElementById("days").textContent = d;     
-           document.getElementById("days").innerText = d; 
-           document.getElementById("hours").textContent = h;     
-           document.getElementById("minutes").textContent = m;     
-           document.getElementById("seconds").textContent = s;     
+            document.getElementById("days").textContent = d;
+            document.getElementById("days").innerText = d;
+            document.getElementById("hours").textContent = h;
+            document.getElementById("minutes").textContent = m;
+            document.getElementById("seconds").textContent = s;
 
-           setTimeout(countdown, 1000);
+            setTimeout(countdown, 1000);
 
         }
 
-        countdown(); 
+        countdown();
 
-        $("#mail-chimp").ajaxChimp({
-          url:'https://netlify.us16.list-manage.com/subscribe/post?u=c2469184e53e2b2f2c0ec5601&amp;id=0d51d314af'
-        });
 
     });
-    
-    jQuery(window).load(function(){
-     
-        
 
-        
-    }); 
+    jQuery(window).load(function() {
+
+
+
+
+    });
 
 }(jQuery));
 
+/*
+(function($) { "use strict";
+    jQuery(document).ready(function($) { $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        $('[data-toggle="tooltip"]').tooltip();
 
+        $(".mailchimp-form").ajaxChimp({ url: "http://rayhanuddin.us16.list-manage.com/subscribe/post?u=3a860ef41d2f97e996189bc1a&amp;id=d172e92f46" });
 
-
-
-
-
-
-
-
-
-
+        $(".clock").countdown("2018/12/10", function(event) { $(this).html(event.strftime('<span class="countdown-wrapper"><span class="single-countdown-inner">%w <br><span>weeks</span></span><span class="single-countdown-inner">%d <br><span>days</span></span><span class="single-countdown-inner">%H <br><span>hours</span></span><span class="single-countdown-inner">%M <br><span>minutes</span></span><span class="single-countdown-inner">%S <br><span>seconds</span></span></span>')) }) });
+    jQuery(window).load(function() {}) })(jQuery); */
