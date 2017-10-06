3bfbfeb111bb682c5beeb45ebdef3e006793e0ac
rozena <rrrozyislam@gmail.com>
2017-10-06 18:45:25 +0600

    cafe

--- landing/cafe/assets/js/main.js
+++ landing/cafe/assets/js/main.js
@@ -1,70 +1,79 @@
 (function($) {
     "use strict";
 
-    $(document).ready(function() {
+    jQuery(document).ready(function($) {
+      //slider js
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true,
+            autoply: true,
+            navText: ["<i class=\'fa fa-long-arrow-left\'></i>", "<i class=\'fa fa-long-arrow-right\'></i>"],
+            mouseDrag: false,
 
-        //bootstrap scrollyspy
-        $('body').scrollspy({
-            target: '.navbar-collapse',
-            offset: 60
         });
 
-     //Smooth scroll
-      $("li.smooth-menu a").bind('click', function (event) {
-          var $anchor = $(this);
-          var headerHeight = '60';
-          $("html, body").stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top - headerHeight + "px"
-          }, 1200, 'easeInOutExpo');
-          event.preventDefault();
-      }); 
-
-        //parallax bg
-        $('.parallax-bg, .header-area-bg').scrolly({ bgParallax: true });
-
-        //mobile menu active
-        $(".navbar-toggle").on('click', function() {
-            $("body").addClass("mobile-menu-activated")
+        //slider animation js
+        $(".homepage-slides").on("translated.owl.carousel", function() {
+            $(".slide-text-tablecell p").addClass("animated fadeInUp").css("opacity", "1");
+            $(".single-slide-item img").addClass("animated fadeInRight").css("opacity", "1");
+        });
+        $(".homepage-slides").on("translate.owl.carousel", function() {
+            $(".slide-text-tablecell p").removeClass("animated fadeInUp").css("opacity", "0");
+            $(".single-slide-item img").removeClass("animated fadeInRight").css("opacity", "0");
         });
 
-        $(".main-menu ul li a").on('click', function() {
-            $(".navbar-collapse").removeClass("in");
-        })
+        //wow js
+        new WOW().init();
 
-  
+        //owlcarousel js
+        $(".product-promotions-wrap").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            autoply: true,
+        });
 
-    //menu scrool top stick
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 150) {
-            $('#menu').addClass('menu-bg');
-        } else {
-            $('#menu').removeClass('menu-bg');
-        }
+        //masonry js
+        $(".product-list").masonry();
+
+        //offcanvas menu js
+        $(".menu-trigger").on("click", function() {
+            $(".ofcanvar-menu, .offcanvar-overlay").addClass("active");
+            return false;
+        });
+
+        $(".menu-close, .offcanvar-overlay").on("click", function() {
+            $(".ofcanvar-menu, .offcanvar-overlay").removeClass("active");
+        });
+
+
+        //search js
+        $(".search-trigger").on('click', function() {
+            $(".search-form").toggleClass("active");
+        });
+
+        //scroll to top .    
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 150) {
+                $(".scrolltotop a").fadeIn();
+            } else {
+                $(".scrolltotop a").fadeOut();
+            }
+        });
+        $(".scrolltotop a").click(function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
     });
 
-    //scroll to top .    
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 150) {
-            $(".scrolltotop a").fadeIn();
-        } else {
-            $(".scrolltotop a").fadeOut();
-        }
+    jQuery(window).load(function() {
+
+      // preloader js
+        $(".site-spinner-wrap").fadeOut(5000);
     });
-
-    $(".scrolltotop a").click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
-
-});
-
-jQuery(window).load(function() {
-
-    $(".main-site-wrap").fadeOut(5000);
-
-});
-
 
 }(jQuery));
