6bcd289c7581a285c60288803ae59b156b7a750d
rozena <rrrozyislam@gmail.com>
2017-10-12 05:27:36 +0600

    demo-5

--- countdown/demo-5/assets/js/active.js
+++ countdown/demo-5/assets/js/active.js
@@ -8,13 +8,14 @@
        });
         
        $(".ajaxchimp-form").ajaxChimp({
-       		url: 'http://netlify.us16.list-manage.com/subscribe/post?u=c2469184e53e2b2f2c0ec5601&amp;id=6d85e55c85'
+       		url:'http://netlify.us16.list-manage.com/subscribe/post?u=c2469184e53e2b2f2c0ec5601&amp;id=6d85e55c85'
        }); 
     
     });
     
     jQuery(window).load(function(){
      
+     $(".site-preloader-wrap").fadeOut(5000);
         
         
         
