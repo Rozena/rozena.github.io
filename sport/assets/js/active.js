f0f26952c5ba9d87d29b97b9db9079a584289a5c
rozena <rrrozyislam@gmail.com>
2018-03-08 23:01:08 +0600

    sport

---
+++ sport/assets/js/active.js
@@ -0,0 +1,39 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+     
+        //Slider 
+        $(".slider-wrap").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            dots: true,
+            nav: false
+        });
+        
+      
+        
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
