33e8722bcfd205e38b2c9c66ad58239dc544f6cf
rozena <rrrozyislam@gmail.com>
2018-03-18 15:41:15 +0600

    add new style

--- games/assets/js/active.js
+++ games/assets/js/active.js
@@ -4,23 +4,56 @@
     jQuery(document).ready(function($) {
  
          $(".participent-list-carousel").owlCarousel({
-	        items: 11,
 	        loop: true,
 	        dots: false,
 	        nav: false,
 	        autoplay: false,
-	        margin: 20
+	        margin: 20,
+	        responsive : {
+			  	0 : {
+			  		items: 2	
+			    },
+			    480 : {
+			    	items: 4	
+			    },
+			    768 : {
+			    	items: 7
+			    },
+			    992 : {
+	        		items: 9
+			    },
+			    1170 : {
+	        		items: 11
+			    }
+			}
 
         });
       
          $(".gun-list-carousel").owlCarousel({
-	        items: 7,
 	        loop: true,
 	        dots: false,
 	        nav: true,
 	        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 	        autoplay: false,
-	        margin: 10
+	        margin: 10,
+	        responsive : {
+			  	0 : {
+			  		items: 2	
+			    },
+			    480 : {
+			    	items: 3	
+			    },
+			    768 : {
+			    	items: 5
+			    },
+			    992 : {
+	        		items: 6
+			    },
+			    1170 : {
+	        		items: 7
+			    }
+			}
+
 
         });
       
@@ -31,10 +64,42 @@
 	        nav: true,
 	        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 	        autoplay: false,
-	        margin: 10
+	        margin: 10,	        
+	        responsive : {
+			  	0 : {
+			  		items: 2	
+			    },
+			    480 : {
+			    	items: 4	
+			    },
+			    768 : {
+			    	items: 7
+			    },
+			    992 : {
+	        		items: 9
+			    },
+			    1170 : {
+	        		items: 11
+			    }
+			}
+
+
 
         });
-      
+
+		const ps = new PerfectScrollbar('.offcanbar-admin-box');
+
+		$(".sidebar-icon").on('click', function() {
+			$(".offcanbar-admin-box").addClass("offcanbar-menu-show");
+		});
+		$(".offcanbar-menu-close").on('click', function() {
+			$(".offcanbar-admin-box").removeClass("offcanbar-menu-show");
+		});
+
+
+		$('#menu').slicknav({
+			prependTo:'#mobilemenu-active'
+		});
         
     });
     
