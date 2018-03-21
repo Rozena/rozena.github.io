0b5e4a52dfbcce6820800168e4beebc98691d937
rozena <rrrozyislam@gmail.com>
2018-03-21 12:05:51 +0600

    blank

---
+++ blank/assets/js/active.js
@@ -0,0 +1,38 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+
+        $(".portfolio-project-wrap").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: false,
+            autoplayTimeout: 5000,
+            dots: true,
+            nav: true,
+            navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+
+        });
+        
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
