a4a6f17da5ad17cc15d4bb21749bcb5441710f90
rozena <rrrozyislam@gmail.com>
2018-05-23 16:52:58 +0600

    stream

---
+++ stream/assets/js/active.js
@@ -0,0 +1,29 @@
+(function($) {
+    "use strict";
+
+    $(document).ready(function($) {
+
+
+
+    $(".hero-content-slide").owlCarousel({
+            items: 1,
+            nav: false,
+            dots: true,
+            autoplay : true,
+            loop: true,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+
+        }); 
+
+
+
+
+    });
+
+    $(window).load(function() {
+
+
+    });
+
+}(jQuery));
